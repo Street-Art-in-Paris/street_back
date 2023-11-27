04a317a1555e7bde07edde424d615a029ee5a783
Quentin <91956889+SadeQL@users.noreply.github.com>
2023-11-27 17:27:19 +0100

    fix: configure db connection

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :street_art, StreetArt.Repo,
   username: "quentinlee",
   password: "postgres",
-  hostname: "db",
+  hostname: System.get_env("DB_HOST") || "db",
   database: "street_art_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
